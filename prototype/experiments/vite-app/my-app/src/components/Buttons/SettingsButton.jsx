17d42f55c9c6af5e1a782bcf607612ec01e6dab2
Brent Shambaugh <brent.shambaugh@gmail.com>
2025-07-07 23:00:55 -0500

    move preferences, settingsButton from local styles state to global scss

--- prototype/experiments/vite-app/my-app/src/components/Buttons/SettingsButton.jsx
+++ prototype/experiments/vite-app/my-app/src/components/Buttons/SettingsButton.jsx
@@ -4,37 +4,10 @@ import { FaCog } from 'react-icons/fa';
 
 function SettingsButton() {
   return (
-    /*
-    <button className={buttonStyle}  aria-label="Settings">
-      <FaCog className={iconStyle} />
-    </button> 
-    */
     <button className="button-style"  aria-label="Settings">
        <FaCog className="icon-style" />
      </button> 
-   /* <button style={buttonStyle}>
-      <FaCog style={iconStyle} /> Settings
-    </button> */
   );
 }
-
-// Optional: Basic styles
-/*
-const buttonStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: '8px',
-  padding: '0px 0px',
-  fontSize: '16px',
-  cursor: 'pointer',
-  backgroundColor: 'transparent', // Makes the button transparent
-  border: 'none', // Explicitly no border
-};
-
-const iconStyle = {
-  fontSize: '16px',
-  color: '#808080', // Medium grey for the cogwheel
-};
-*/
 
 export default SettingsButton;
